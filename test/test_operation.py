688e9c5af6239e991d3c1c772980e08acc0c1a75
kiran <kiran.karakalli92@gmail.com>
2025-11-15 12:55:36 +0530

    Unit testcases updated

---
+++ test/test_operation.py
@@ -0,0 +1,16 @@
+from src.math_operation import add,sub
+
+def test_add():
+    assert add(2,3)==5
+    assert add(-1,1)==0
+    assert add(5,3)==2
+
+
+def test_sub():
+    assert sub(2,2)==0
+    assert sub(2,1)==1
+    assert sub(1,2)==0
+
+
+
+
